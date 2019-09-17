49c7b034dedcebf24dc0a47e65784aecad3feb33
Shizue Chalwell <shizue46@gmail.com>
2019-09-17 22:45:06 +0000

    resolution

--- app/controllers/instructor/lessons_controller.rb
+++ app/controllers/instructor/lessons_controller.rb
@@ -12,6 +12,8 @@ class Instructor::LessonsController < ApplicationController
     redirect_to instructor_course_path(current_section.course)
   end 
 
+
+
 private 
 
   def require_authorized_for_current_section
