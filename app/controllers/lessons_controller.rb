f0369b7eed14542954b4ee8c081ff7f00625341c
Shizue Chalwell <shizue46@gmail.com>
2019-10-04 07:14:45 +0000

    Reorder lessons

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,27 +1,28 @@
 class LessonsController < ApplicationController 
   before_action :authenticate_user!
-  before_action :require_authorized_for_current_lesson
+  before_action :require_authorized_for_current_course
 
   def show 
-    
   end 
 
   private
   
-  def require_authorized_for_current_lesson
-    if current_user.enrolled_in?(current_course)!= current_user
-      redirect_to course_url, alert: 'Not Enrolled'
-      #redirect_to "/"
+  def require_authorized_for_current_course
+    if current_course.user != current_user
+      #redirect_to course_path, alert: 'Not Enrolled'
+      redirect_to "/"
     end 
   end
 
-  #helper_method :current_lesson 
-  #def current_lesson 
-    #@current_lesson.section.course ||= Lesson.find(params[:id])
-
   helper_method :current_course
-    def current_course
-      @current_course ||= Course.find(params[:id])
-    end
+
+  def current_course 
+    @current_lesson.section.course ||= Lesson.find(params[:id])
+  end 
+
+  #helper_method :current_course
+    #def current_course
+      #@current_course ||= Course.find(params[:id])
+    #end
   #end 
 end
